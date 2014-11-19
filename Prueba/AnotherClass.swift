e7a4f4996cc9b0e09355c1a723988b3e5bc6d9a9
fenixof <fenix_of@msn.com>
2014-11-19 22:06:42 +0100

    Añadida otra clase

---
+++ Prueba/AnotherClass.swift
@@ -0,0 +1,13 @@
+//
+//  AnotherClass.swift
+//  Prueba
+//
+//  Created by David Ortiz Fuentes on 19/11/14.
+//  Copyright (c) 2014 David Ortiz Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class AnotherClass: NSObject {
+   
+}
